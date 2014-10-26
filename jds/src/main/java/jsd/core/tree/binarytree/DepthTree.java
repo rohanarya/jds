f59b287a30874b38123185602eb1dcb6caed1beb
rohanarya <rohanarya85@gmail.com>
2014-10-26 13:56:06 +0530

    Depth of a Binary Tree

---
+++ jds/src/main/java/jsd/core/tree/binarytree/DepthTree.java
@@ -0,0 +1,20 @@
+package jsd.core.tree.binarytree;
+
+import jsd.core.tree.TreeNode;
+
+/**
+ * This is a program to find the depth of Binary Tree
+ * 
+ * @author rohanarya
+ * 
+ */
+public class DepthTree {
+
+	public int depth(final TreeNode<Integer> root) {
+		if(root == null){
+			
+		}
+		return 0;
+	}
+
+}

2578dcc4be3fc537011773fbae5d960b21bc81f6
rohanarya <rohanarya85@gmail.com>
2014-10-26 21:35:46 +0530

    Adding some comments for Diameter

--- jds/src/main/java/jsd/core/tree/binarytree/DiameterOfTree.java
+++ jds/src/main/java/jsd/core/tree/binarytree/DiameterOfTree.java
@@ -4,7 +4,10 @@ import jsd.core.tree.TreeNode;
 
 /**
  * Amazon question - Calculates the daimeter of a tree which is basically the
- * length of the path between two leaves.
+ * length of the path between two leaves. The scenarios to consirder are
+ * --------------------1) When the longest path is via the root
+ * --------------------2) The longest path exists in the right subtree
+ * --------------------3) The longest path exists in the left subtree
  * 
  * For e.g. the diameter of {@link TreeFactory#createIntTree()} is 6 i.e
  * 

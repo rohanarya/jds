9f903bcacfa045cd2e6ec5533455f3e593cee2b2
rohanarya <rohanarya85@gmail.com>
2014-10-26 19:09:55 +0530

    Diameter Class

---
+++ jds/src/main/java/jsd/core/tree/binarytree/DiameterOfTree.java
@@ -0,0 +1,21 @@
+package jsd.core.tree.binarytree;
+
+import jsd.core.tree.TreeNode;
+
+/**
+ * Amazon question - Calculates the daimeter of a tree which is basically the
+ * length of the path between two leaves.
+ * 
+ * For e.g. the diameter of {@link TreeFactory#createIntTree()} is 6 i.e
+ * 
+ * 4 - 2 - 1 -3 - 6 - 7
+ * 
+ * @author rohanarya
+ * 
+ */
+public class DiameterOfTree<T> {
+
+	public int diameter(TreeNode<T> root) {
+		return 0;
+	}
+}
